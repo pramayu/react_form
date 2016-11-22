b357c69b890fb849a67c594e32f63e347aea7c31
pramayu <gedepramayu@gmail.com>
2016-11-22 15:12:53 +0700

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var EmailField = require('./components/EmailField.jsx');
+
+
+ReactDOM.render(<EmailField />, document.getElementById('ingredients'));
